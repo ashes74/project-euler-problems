414bf6f57685fb9ecdab7228f17b7bc4bb60c558
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-03-23 14:03:30 -0400

    Add Euler problem 1 - Python solution and README.md

---
+++ Euler.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python -tt
+# Lori-Anne Ashwood
+# Using Python to solve Euler projects 
+
+#Euler projects
+#https://projecteuler.net/
+
+import sys
+import time
+
+#Euler Problem 1
+# https://projecteuler.net/
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+def sum_of_multiples(limit):
+	total = 0
+	for n in range (limit):
+		if (n%3==0 or n%5==0):
+			total+=n
+	return total
+
+def sum_of_mul_2 (limit):
+	thislist = [n for n in range (limit) if n%3==0 or n%5==0]
+	total = sum(thislist)
+	return total
+
+def timeitnow(fcnx):
+	timestart = time.time()
+	print "Sum is" , fcnx
+	timeend = time.time()
+	return timeend - timestart
+
+
+def main():
+	print "Euler Problem 1:"
+	print sum_of_multiples(1000)
+
+	print "Take 2:"
+	print sum_of_mul_2(1000)
+
+	print "Time it 1:"
+	total1 = timeitnow (sum_of_multiples(1000))
+	print total1
+
+	print "Time it 2:"
+	total2 = timeitnow (sum_of_mul_2(1000))
+
+	print total2
+
+	if total1<total2:
+		print "range is faster"
+	elif total2<total1:
+		print "list is faster"
+	else:
+		print "they are equal!"
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+
