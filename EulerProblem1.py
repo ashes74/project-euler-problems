2fb61c6f4a07a036659c76600dc4a84519d2c612
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-03-23 15:19:31 -0400

    Add Project Euler problem 1 with time comparison

--- EulerProblem1.py
+++ EulerProblem1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python -tt
 # Lori-Anne Ashwood
-# Using Python to solve Euler projects 
+# Using Python to solve Euler projects
 
 #Euler projects
 #https://projecteuler.net/
@@ -10,7 +10,7 @@ import time
 
 #Euler Problem 1
 # https://projecteuler.net/
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
@@ -36,10 +36,10 @@ def timeitnow(fcnx):
 
 def main():
 	print "Euler Problem 1:"
-	print sum_of_multiples(1000)
+	print sum_of_multiples(1000) #expect 23
 
 	print "Take 2:"
-	print sum_of_mul_2(1000)
+	print sum_of_mul_2(1000) #expect 23
 
 	print "Time it 1:"
 	total1 = timeitnow (sum_of_multiples(1000))
@@ -64,4 +64,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
